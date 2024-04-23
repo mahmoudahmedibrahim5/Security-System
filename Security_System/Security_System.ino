066312fdd26c3347aa4c3e18b7936f94b87d90e8
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-04-23 19:53:19 +0200

    Edit messages to fit + Add 3 more messages

--- Security_System/Security_System.ino
+++ Security_System/Security_System.ino
@@ -7,21 +7,21 @@ LiquidCrystal_I2C lcdDown(0x23, 40, 2);
 #define BUZZER    53
 
 /* Global variables */
-bool pressed [30];
+bool pressed [33];
 int index;
-int pressedIndicies[30];
+int pressedIndicies[33];
 int pressedCount;
 int oldCount;
 
 /* Messages to be displayed on lcdUp */
 char countMessage[21] = "Open Doors Count=   "; // Length = 20
 char ignition[21] = "    IGNITION ON     ";
-String messages[30] = 
+String messages[33] = 
 {
   "Entry Door",
   "Motorhome Slide",
   "Skylight",
-  "Skylight Opaque Cover Screen isn't Retracted",
+  "Skylight Opaque Screen not Retracted",
   "Skylight Insect Screen is not Retracted",
   "Right Kitchen Overhead Cupboard Door",
   "Left Kitchen Overhead Cupboard Door",
@@ -42,12 +42,15 @@ String messages[30] =
   "Middle Floor-Level Draw",
   "Right Floor-Level Draw",
   "Bathroom Siding Door",
-  "Right Bathroom Under-Vanity Door (Water Heater Area)",
+  "Right Under-Vanity Door (HWS)",
   "Left Bathroom Under-Vanity Door",
   "Right Over-Head Locker Door",
   "Centre Over-Head Locker Door",
   "Left Over-Head Locker Door",
-  "Bathrooms Window"
+  "Bathrooms Window",
+  "Shore Power is still connected",
+  "Driver-Side Locker",
+  "Passenger-Side Locker"
 };
 
 /* Functions prototype */
@@ -67,6 +70,9 @@ void setup()
   /* Initialize input pins */
   for(int i = 22; i < 52; i++)
     pinMode(i, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
 
   /* Initialize the buzzer */
   pinMode(BUZZER, OUTPUT);
@@ -119,6 +125,25 @@ void checkDoors(void)
       pressedIndicies[index++] = i - 22;
     }
   }
+  
+  pressed[30] = digitalRead(A0);
+  if(pressed[30]){
+    pressedCount++;
+    pressedIndicies[index++] = 30;
+  }
+  
+  pressed[31] = digitalRead(A1);
+  if(pressed[31]){
+    pressedCount++;
+    pressedIndicies[index++] = 31;
+  }
+  
+  pressed[32] = digitalRead(A2);
+  if(pressed[32]){
+    pressedCount++;
+    pressedIndicies[index++] = 32;
+  }
+
   if(oldCount < pressedCount){
     digitalWrite(BUZZER, HIGH);
     delay(500);
