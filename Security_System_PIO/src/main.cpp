1aa9a5a65325a1ca7b80bfc2c266b8e8477e3ab6
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2024-06-03 03:46:06 +0300

    Update the last changes in arduino code

--- Security_System_PIO/src/main.cpp
+++ Security_System_PIO/src/main.cpp
@@ -1,28 +1,30 @@
 #include <Arduino.h>
-#include "LiquidCrystal_I2C.h"
+#include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcdUp(0x27, 40, 2);
-LiquidCrystal_I2C lcdDown(0x23, 40, 2);
+//LiquidCrystal_I2C lcdDown(0x26, 40, 2);
 
-#define IGNITION  52
-#define BUZZER    53
+#define IGNITION      52
+#define BUZZER        53
+#define DELAY         2000
+#define BUZZER_DELAY  100   // Delay of the ignition 10 beeps
 
 /* Global variables */
-bool pressed [30];
+bool pressed [33];
 int index;
-int pressedIndicies[30];
+int pressedIndicies[33];
 int pressedCount;
 int oldCount;
 
 /* Messages to be displayed on lcdUp */
 char countMessage[21] = "Open Doors Count=   "; // Length = 20
 char ignition[21] = "    IGNITION ON     ";
-String messages[30] = 
+String messages[33] = 
 {
   "Entry Door",
   "Motorhome Slide",
   "Skylight",
-  "Skylight Opaque Cover Screen isn't Retracted",
+  "Skylight Opaque Screen not Retracted",
   "Skylight Insect Screen is not Retracted",
   "Right Kitchen Overhead Cupboard Door",
   "Left Kitchen Overhead Cupboard Door",
@@ -43,18 +45,23 @@ String messages[30] =
   "Middle Floor-Level Draw",
   "Right Floor-Level Draw",
   "Bathroom Siding Door",
-  "Right Bathroom Under-Vanity Door (Water Heater Area)",
+  "Right Under-Vanity Door (HWS)",
   "Left Bathroom Under-Vanity Door",
   "Right Over-Head Locker Door",
   "Centre Over-Head Locker Door",
   "Left Over-Head Locker Door",
-  "Bathrooms Window"
+  "Bathrooms Window",
+  "Shore Power is still connected",
+  "Driver-Side Locker",
+  "Passenger-Side Locker"
 };
 
 /* Functions prototype */
 void checkIgnition(void);
 void checkDoors(void);
+void newDoorBuzzer(void);
 void displayPressedCount(void);
+void displayBacklight(void);
 void displayOpenedDoors(void);
 
 void setup() 
@@ -62,12 +69,17 @@ void setup()
   /* Initialize the lcdUp */
   lcdUp.init();
   lcdUp.clear();
-  lcdDown.init();
-  lcdDown.clear();
+  lcdUp.backlight();
+  //lcdDown.init();
+  //lcdDown.clear();
+  //lcdDown.backlight();
 
   /* Initialize input pins */
   for(int i = 22; i < 52; i++)
     pinMode(i, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
 
   /* Initialize the buzzer */
   pinMode(BUZZER, OUTPUT);
@@ -84,23 +96,47 @@ void loop()
   /* Check the doors */
   checkDoors();
 
+  /* Buzzer if new door is opened */
+  newDoorBuzzer();
+
   /* Edit the display pressed Count */
   displayPressedCount();
+
+  /* Update the backlight */
+  displayBacklight();
 
   /* Edit the messages of the doors */
   displayOpenedDoors();
 }
 
+/*************************************************************************************************/
+/************************************* Functions Declaration *************************************/
+/*************************************************************************************************/
+
 void checkIgnition(void)
 {
+  int beeps = 0;
   while (!digitalRead(IGNITION))
   {
+    checkDoors();
+    displayBacklight();
     lcdUp.setCursor(0, 0);
     lcdUp.print(ignition);
-    digitalWrite(BUZZER, HIGH);
-    delay(100);
-    digitalWrite(BUZZER, LOW);
-    delay(100);
+    if(beeps >= 10)
+    {
+      digitalWrite(BUZZER, HIGH);
+      delay(DELAY);
+      digitalWrite(BUZZER, LOW);
+      delay(DELAY);
+    }
+    else
+    {
+      digitalWrite(BUZZER, HIGH);
+      delay(BUZZER_DELAY);
+      digitalWrite(BUZZER, LOW);
+      delay(BUZZER_DELAY);
+      beeps++;
+    }
   }
 }
 
@@ -116,6 +152,29 @@ void checkDoors(void)
       pressedIndicies[index++] = i - 22;
     }
   }
+  
+  pressed[30] = digitalRead(A0);
+  if(pressed[30]){
+    pressedCount++;
+    pressedIndicies[index++] = 30;
+  }
+  
+  pressed[31] = digitalRead(A1);
+  if(pressed[31]){
+    pressedCount++;
+    pressedIndicies[index++] = 31;
+  }
+  
+  pressed[32] = digitalRead(A2);
+  if(pressed[32]){
+    pressedCount++;
+    pressedIndicies[index++] = 32;
+  }
+
+}
+
+void newDoorBuzzer(void)
+{
   if(oldCount < pressedCount){
     digitalWrite(BUZZER, HIGH);
     delay(500);
@@ -135,6 +194,14 @@ void displayPressedCount(void)
   lcdUp.print(countMessage);
 }
 
+void displayBacklight(void)
+{
+  if(pressedCount > 0)
+    lcdUp.backlight();
+  else
+    lcdUp.noBacklight();
+}
+
 void displayOpenedDoors(void)
 {
   index = 0;
@@ -142,33 +209,17 @@ void displayOpenedDoors(void)
   {
     lcdUp.setCursor(0, 1);
     lcdUp.print(messages[pressedIndicies[index++]]);
-    delay(1000);
+    delay(DELAY);
     lcdUp.clear();
-    lcdDown.clear();
   }
-  else if(pressedCount == 2)
+  else if(pressedCount > 1)
   {
     lcdUp.setCursor(0, 1);
     lcdUp.print(messages[pressedIndicies[index++]]);
-    lcdDown.setCursor(0, 0);
-    lcdDown.print(messages[pressedIndicies[index++]]);
-    delay(1000);
+    delay(DELAY);
     lcdUp.clear();
-    lcdDown.clear();
-  }
-  else if(pressedCount >= 3)
-  {
-    lcdUp.setCursor(0, 1);
-    lcdUp.print(messages[pressedIndicies[index++]]);
-    lcdDown.setCursor(0, 0);
-    lcdDown.print(messages[pressedIndicies[index++]]);
-    lcdDown.setCursor(0, 1);
-    lcdDown.print(messages[pressedIndicies[index++]]);
-    delay(1000);
-    lcdUp.clear();
-    lcdDown.clear();
 
-    pressedCount -= 3;
+    pressedCount--;
     while (pressedCount > 0)
     {
       if(pressedCount){
@@ -181,19 +232,71 @@ void displayOpenedDoors(void)
         lcdUp.print(messages[pressedIndicies[index++]]);
         pressedCount--;
       }
-      if(pressedCount){
-        lcdDown.setCursor(0, 0);
-        lcdDown.print(messages[pressedIndicies[index++]]);
-        pressedCount--;
-      }
-      if(pressedCount){
-        lcdDown.setCursor(0, 1);
-        lcdDown.print(messages[pressedIndicies[index++]]);
-        pressedCount--;
-      }
-      delay(1000);
+      delay(DELAY);
       lcdUp.clear();
-      lcdDown.clear();
     }
   }
 }
+
+// void displayOpenedDoors(void)
+// {
+//   index = 0;
+//   if(pressedCount == 1)
+//   {
+//     lcdUp.setCursor(0, 1);
+//     lcdUp.print(messages[pressedIndicies[index++]]);
+//     delay(DELAY);
+//     lcdUp.clear();
+//     lcdDown.clear();
+//   }
+//   else if(pressedCount == 2)
+//   {
+//     lcdUp.setCursor(0, 1);
+//     lcdUp.print(messages[pressedIndicies[index++]]);
+//     lcdDown.setCursor(0, 0);
+//     lcdDown.print(messages[pressedIndicies[index++]]);
+//     delay(DELAY);
+//     lcdUp.clear();
+//     lcdDown.clear();
+//   }
+//   else if(pressedCount >= 3)
+//   {
+//     lcdUp.setCursor(0, 1);
+//     lcdUp.print(messages[pressedIndicies[index++]]);
+//     lcdDown.setCursor(0, 0);
+//     lcdDown.print(messages[pressedIndicies[index++]]);
+//     lcdDown.setCursor(0, 1);
+//     lcdDown.print(messages[pressedIndicies[index++]]);
+//     delay(DELAY);
+//     lcdUp.clear();
+//     lcdDown.clear();
+
+//     pressedCount -= 3;
+//     while (pressedCount > 0)
+//     {
+//       if(pressedCount){
+//         lcdUp.setCursor(0, 0);
+//         lcdUp.print(messages[pressedIndicies[index++]]);
+//         pressedCount--;
+//       }
+//       if(pressedCount){
+//         lcdUp.setCursor(0, 1);
+//         lcdUp.print(messages[pressedIndicies[index++]]);
+//         pressedCount--;
+//       }
+//       if(pressedCount){
+//         lcdDown.setCursor(0, 0);
+//         lcdDown.print(messages[pressedIndicies[index++]]);
+//         pressedCount--;
+//       }
+//       if(pressedCount){
+//         lcdDown.setCursor(0, 1);
+//         lcdDown.print(messages[pressedIndicies[index++]]);
+//         pressedCount--;
+//       }
+//       delay(DELAY);
+//       lcdUp.clear();
+//       lcdDown.clear();
+//     }
+//   }
+// }
